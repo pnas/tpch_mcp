ad498e0b8d97cee31a5525e46a72cb89d522c2eb
thanasis <thanasispapaioan@gmail.com>
2025-07-02 22:36:34 +0300

    docker

--- tpch_api/config.py
+++ tpch_api/config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://user:password@localhost/tpch')
